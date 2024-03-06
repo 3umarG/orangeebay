ad5e629cf39c4b1fd372b77cb7e522465834e5f6
OmarGomaaFCi <omargomaafcitu@gmail.com>
2024-03-06 18:04:30 +0200

    add about us CRUD

---
+++ Orange.Api/Controllers/AboutUsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orange_Bay.DTOs.Auth;
+using Orange.EF.Services;
+
+namespace Orange.Api.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class AboutUsController
+{
+    [HttpGet]
+    public IActionResult GetAboutUs()
+    {
+        var result =  AboutUsService.GetAboutUs();
+        var response = ResponseModelDto<string>.BuildSuccessResponse(result);
+        return new OkObjectResult(response);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateAboutUs([FromBody] AboutUsRequestBody requestBody)
+    {
+        AboutUsService.Update(requestBody.Text);
+        var result = AboutUsService.GetAboutUs();
+        var response = ResponseModelDto<string>.BuildSuccessResponse(result);
+        return new OkObjectResult(response);
+    }
+
+}
+
+public class AboutUsRequestBody
+{
+    public string Text { get; set; }
+}
---
+++ Orange.EF/Services/AboutUsService.cs
@@ -0,0 +1,13 @@
+﻿namespace Orange.EF.Services;
+
+public class AboutUsService
+{
+    private static string _aboutUsText = "";
+
+    public static void Update(string text)
+    {
+        _aboutUsText = text;
+    }
+
+    public static string GetAboutUs() => _aboutUsText;
+}
